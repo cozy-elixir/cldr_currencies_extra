75f80ca17f2e7084a9dbb99f0eb7fe83d72bce98
c4710n <c4710n@users.noreply.github.com>
2023-11-26 15:27:20 +0800

    init: mix new ex_cldr_currencies_extra --app ex_cldr_currencies_extra

---
+++ lib/cldr.ex
@@ -0,0 +1,18 @@
+defmodule Cldr do
+  @moduledoc """
+  Documentation for `Cldr`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cldr.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cldr_test.exs
@@ -0,0 +1,8 @@
+defmodule CldrTest do
+  use ExUnit.Case
+  doctest Cldr
+
+  test "greets the world" do
+    assert Cldr.hello() == :world
+  end
+end
