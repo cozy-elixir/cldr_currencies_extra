fa46c4948b1eeba2b5af7c0e06a8160f3f779021
c4710n <c4710n@users.noreply.github.com>
2023-11-27 21:06:01 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Cldr.Currencies.Extra.MixProject do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
   @description "Extra utils for Cldr.Currency."
   @source_url "https://github.com/cozy-elixir/cldr_currencies_extra"
 
